8e7f92a64da4258bce5fee5f891e0026c4d9005c
pugal001 <pugal71182pugal@gmail.com>
2024-06-11 18:47:22 +0530

    11thjune

--- django_full_course/response/urls.py
+++ django_full_course/response/urls.py
@@ -1,8 +1,24 @@
-from response.views import basic_response, api_response, html_fetcher
+from response.views import (
+    basic_response,
+    api_response,
+    html_fetcher,
+    redicrector,
+    file_handler,
+    temp_response,
+    page_not_found,
+    bad_request,
+    page_not_allowed,
+)
 from django.urls import path
 
 urlpatterns = [
     path("basic", basic_response),
     path("api", api_response),
     path("html", html_fetcher),
+    path("redirect", redicrector),
+    path("temp", temp_response),
+    path("file", file_handler),
+    path("not_found", page_not_found),
+    path("bad", bad_request),
+    path("not_allowed", page_not_allowed),
 ]
--- django_full_course/response/views.py
+++ django_full_course/response/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import (
+    HttpResponse,
+    JsonResponse,
+    FileResponse,
+    HttpResponseBadRequest,
+    HttpResponseNotAllowed,
+    HttpResponseNotFound,
+    HttpResponseServerError,
+)
+from django.template.response import TemplateResponse
+import datetime
 
 
 # Create your views here.
@@ -15,3 +25,39 @@ def api_response(request):
 
 def html_fetcher(request):
     return render(request, "index.html")
+
+
+def redicrector(request):
+    return redirect("https://en.wikipedia.org/wiki/Django_(web_framework)")
+
+
+def temp_response(request):
+    now = datetime.datetime.now()
+    time_dict = {"year": now.year, "month": now.month, "day": now.day, "hour": now.hour}
+    return TemplateResponse(request, "time.html", context=time_dict)
+
+
+def file_handler(request):
+    file_path = "C:/User_Desk/Teaching_contents/Django_teaching_documents/Django_Full_Course_project/django_full_course/shiva_1.jpg"
+
+    response = FileResponse(open(file_path, "rb"))
+
+    response["Content-Type"] = "application/jpg"
+    response["Content-Disposition"] = 'attachement; filename = "sample.jpg"'
+    return response
+
+
+def page_not_found(request):
+    return HttpResponseServerError("server busy")
+
+
+def bad_request(request):
+    return HttpResponseBadRequest("seems like its a bad request")
+
+
+def page_not_allowed(request):
+    return HttpResponseNotFound("you are not allowed to view this page")
+
+
+def page_not_found(request):
+    return HttpResponseServerError("server busy")
